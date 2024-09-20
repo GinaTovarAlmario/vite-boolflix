29f00fc7d1082f4ca398c6d0c24b76f3fa6fb7f4
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-09-20 18:43:15 +0200

    added first scheme  general structure of my app

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,21 @@
 <script>
+import HeaderSearchBar from './components/HeaderSearchBar.vue';
+export default{
+  data(){
+    return{
+
+    }
+  },
+  components:{
+    HeaderSearchBar
+  }
+}
 </script>
 
 <template>
- 
+  <HeaderSearchBar/>
 </template>
 
 <style lang="scss">
+@use "./style/general.scss"
 </style>
---
+++ src/components/HeaderSearchBar.vue
@@ -0,0 +1,27 @@
+<script>
+export default{
+    data(){
+        return{
+
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="search">
+        <input type="text">
+        <button>cerca film</button>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.search{
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    gap :10px;
+    padding-right: 30px;
+    margin-top: 40px;
+}
+</style>
