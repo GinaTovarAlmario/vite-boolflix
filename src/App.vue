12759568b869b379c884913f7c69637d0993869d
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-09-20 19:59:51 +0200

    got information about movies and inserted in the template.  added some basic style for the list

--- src/App.vue
+++ src/App.vue
@@ -45,8 +45,40 @@ export default{
 <template>
   <HeaderSearchBar
     @searchItem="searchElement"/>
+    <main>
+      <!-- store.items potrebbe essere null data l'impostazione che ho datp -->
+      <ul class="movies" v-if=" store.items !== null ">
+        <li v-for="(item,index) in store.items.results ":key="index">
+              <p>TITOLO: {{ item.title }}</p>
+              <p>TITOLO ORIGINALE: {{ item.original_title }}</p>
+              <p>LINGUA: {{ item.original_language }}</p>
+              <p>VOTO: {{ item.vote_average }}</p>
+        </li>
+      </ul>
+    </main>
 </template>
 
 <style lang="scss">
-@use "./style/general.scss"
+@use "./style/general.scss";
+  .movies{
+    list-style-type: none;
+    display: flex;
+    flex-wrap: wrap;
+    align-items: baseline;
+    justify-content: center;
+    gap: 5px;
+    padding: 50px 0;
+
+    li{
+      flex-basis: calc(100% / 6 - 5px);
+      padding: 10px 10px;
+
+      p{
+        margin-bottom: 5px;
+      }
+    }
+
+
+  }
+
 </style>
