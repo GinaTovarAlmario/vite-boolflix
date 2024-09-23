bd939bfb1dd683f69e6593c3ebdb1aee273a969e
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-09-23 17:13:26 +0200

    added fontawesome

--- src/App.vue
+++ src/App.vue
@@ -55,5 +55,7 @@ export default{
 
 <style lang="scss">
 @use "./style/general.scss";
+@import '~@fortawesome/fontawesome-free/scss/fontawesome';
+@import '~@fortawesome/fontawesome-free/scss/solid';
 
 </style>
