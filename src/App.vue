9ca5050f3fa89f50106db5ed1e3195537cd89291
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-09-23 10:34:36 +0200

    added appMain component, then displayed the result of seacrh movies inside the page

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script>
 import HeaderSearchBar from './components/HeaderSearchBar.vue';
+import AppMain from './components/AppMain.vue';
 import axios from 'axios';
 import { store } from './store';
 export default{
@@ -12,7 +13,8 @@ export default{
     }
   },
   components:{
-    HeaderSearchBar
+    HeaderSearchBar,
+    AppMain,
   },
   methods:{
     searchElement(message){
@@ -38,6 +40,9 @@ export default{
       });
     }
   },
+  created(){
+    this.getData('');
+  }
 
 }
 </script>
@@ -45,40 +50,10 @@ export default{
 <template>
   <HeaderSearchBar
     @searchItem="searchElement"/>
-    <main>
-      <!-- store.items potrebbe essere null data l'impostazione che ho datp -->
-      <ul class="movies" v-if=" store.items !== null ">
-        <li v-for="(item,index) in store.items.results ":key="index">
-              <p>TITOLO: {{ item.title }}</p>
-              <p>TITOLO ORIGINALE: {{ item.original_title }}</p>
-              <p>LINGUA: {{ item.original_language }}</p>
-              <p>VOTO: {{ item.vote_average }}</p>
-        </li>
-      </ul>
-    </main>
+    <AppMain/>
 </template>
 
 <style lang="scss">
 @use "./style/general.scss";
-  .movies{
-    list-style-type: none;
-    display: flex;
-    flex-wrap: wrap;
-    align-items: baseline;
-    justify-content: center;
-    gap: 5px;
-    padding: 50px 0;
-
-    li{
-      flex-basis: calc(100% / 6 - 5px);
-      padding: 10px 10px;
-
-      p{
-        margin-bottom: 5px;
-      }
-    }
-
-
-  }
 
 </style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,46 @@
+<script>
+import { store } from '../store';
+export default{
+    data(){
+        return{
+            store,
+        }
+    }
+}
+</script>
+<template>
+        <main>
+            <div class="container">
+                <!-- store.items potrebbe essere null data l'impostazione che ho datp -->
+                <ul class="movies" v-if=" store.items !== null ">
+                    <li v-for="(item,index) in store.items.results ":key="index">
+                        <p>TITOLO: {{ item.title }}</p>
+                        <p>TITOLO ORIGINALE: {{ item.original_title }}</p>
+                        <p>LINGUA: {{ item.original_language }}</p>
+                        <p>VOTO: {{ item.vote_average }}</p>
+                    </li>
+                </ul>
+            </div>
+    </main>
+</template>
+<style lang="scss" scoped>
+.movies{
+    list-style-type: none;
+    display: flex;
+    flex-wrap: wrap;
+    align-items: baseline;
+    justify-content: center;
+    gap: 5px;
+    padding: 50px 0;
+
+    li{
+      flex-basis: calc(100% / 6 - 5px);
+      padding: 10px 10px;
+
+      p{
+        margin-bottom: 5px;
+      }
+    }
+}
+
+</style>
