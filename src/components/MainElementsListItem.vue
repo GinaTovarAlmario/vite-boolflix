8c94a5b3960f7f416e05a850805c0b6edfb2bebc
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-09-24 15:59:53 +0200

    added effects on hover. reorganized the card disposition, height and changed some colors.

--- src/components/MainElementsListItem.vue
+++ src/components/MainElementsListItem.vue
@@ -4,7 +4,7 @@ export default{
     data(){
         return{
             store,
-            imgUrl:'https://image.tmdb.org/t/p/w342/',
+            imgUrlPoster:'https://image.tmdb.org/t/p/w342/',
             flagLanguage:{                
                 de:'de.gif',
                 es:'es.gif',
@@ -16,6 +16,8 @@ export default{
                 tr:'tr.gif',
                 en:'en.gif',
                 zh:'zh.gif',
+                it:'it.gif',
+                fr:'fr.gif',
             }
             
 
@@ -43,56 +45,89 @@ export default{
 <template>
     <div class="col" v-for="(item,index) in store.items.results" :key="index">
         <div class="card">
-            <img :src="this.imgUrl + item.poster_path" :alt="item.title || item.name">
-            <p>Titolo: 
-                {{ item.title || item.name}}
-            </p>
-            <p>Titolo originale:
-                {{ item.original_title || item.original_name}}
-            </p>
-            <p>lingua originale:
-                <img class="flag" :src="getPathFlag(item.original_language)" alt="..">
-            </p>
-            <div class="stars-wrapper">
-                <p>voto:
-                    <i class="fa-solid fa-star" v-for="n in getNumberStarVote(item.vote_average)" :key="n"></i>
-                </p>
+            <div class="card-img">
+                <img :src="this.imgUrlPoster + item.poster_path " :alt="item.title || item.name">
+            </div>
+            <div class="card-info">
+
+                <h4>Titolo: </h4>
+                <p>{{ item.title || item.name}}</p>
+
+                <h4>Titolo originale:</h4>
+                <p>{{ item.original_title || item.original_name}}</p> 
+            
+                <h4>lingua originale:</h4>
+                <!-- <div class="flag"> -->
+                    <img class="flag" :src="getPathFlag(item.original_language)" alt="..">
+                <!-- </div> -->
+
+                <div class="stars-wrapper">
+                    <h4>voto:</h4>
+                    <span>
+                        <i class="fa-solid fa-star" v-for="n in getNumberStarVote(item.vote_average)" :key="n"></i>
+                    </span>
+                </div>
             </div>
         </div>
     </div>
 </template>
 <style lang="scss" scoped>
     .col{
-        flex-basis: calc(100% / 6 - 10px);
+        flex-basis: calc(100% / 4 - 10px);
+
 
         .card{
-            height: 600px;
             overflow: hidden;
+            height: 360px;
             width: 100%;
             border: 1px solid white;
             font-size: 16px;
             margin-bottom: 30px;
-            img{
-                width: 100%;
-                height: auto;
+            cursor: pointer;
+        
+
+            .card-img{
+                height: 360px;
+
+                img{
+                    width: 100%;
+                    height: auto;
+                }
             }
-            
-            p{
-                padding: 5px 10px;
+            .card-info{
+                height: 100%;
+                display: none;
+                color: white;
+                background-color: black;
+                font-size: 16px;
+                text-align: center;
+                h4{
+                    padding: 10px 10px;
+                }
                 .flag{
                     height: 20px;
-                    width: 30px;
-                    margin: 10px 5px;
+                     width: 30px;
                 }
                 .stars-wrapper{
                     display: flex;
                     align-items: center;
                     justify-content: center;
-                  
-                }
-            }
-        
-        }
+                    .fa-solid{
+                        color: yellow;
+                    }
 
+                }
+                
+            }
+           
+        }
+        .card:hover{
+            .card-img{
+                display: none;
+            }
+            .card-info{
+                display: block;
+            }
+        }
     }
 </style>
